cf9c9aaa82f51268d0b7e51b6334b108f624ece4
Xavi <149520332+llotjer@users.noreply.github.com>
2024-06-27 13:13:23 +0200

    Changes in url and exercice1 from module 2

---
+++ TEMA_2/exercici1.php
@@ -0,0 +1,12 @@
+<?php 
+
+$cognom = "Reynal";
+$edat = 40;
+$pi = 3.1416;
+$boolean = true;
+
+echo $cognom . "<br>", $edat . "<br>", $pi . "<br>", $boolean;
+
+const NOM = "Xavi";
+
+echo "<h1>" . NOM . "</h1>";
