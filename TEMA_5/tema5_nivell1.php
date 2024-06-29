b1918a55f5c60412d00c85f9c14ede6abbb2fa53
Xavi <149520332+llotjer@users.noreply.github.com>
2024-06-29 15:42:13 +0200

    Added getters, setters, and coments

--- TEMA_5/tema5_nivell1.php
+++ TEMA_5/tema5_nivell1.php
@@ -1,24 +1,40 @@
 <?php
 
 abstract class Animal{
-
+    //PROPERTIES
     public $cry; //This is the default word for the sound of any animal.
 
+    //CONSTRUCTOR
     public function __construct(string $cry){
         $this->cry = $cry;
     }
 
+    //GETTERS
+
+    public function getCry(): string{
+        return $this->cry;
+    }
+
+    //SETTERS
+
+    public function setCry(string $cry){
+        $this->cry = $cry;
+    }
+
+    //METHOD
     public abstract function toCry();
 
 }
 
 class Dog extends Animal{
-
+    //CONSTRUCTOR
     public function __construct($cry){
         parent::__construct($cry);
     }
+
+    //METHOD
     public function toCry(): string{
-        return 'BuP, BUP!';
+        return 'BUP, BUP!';
     }
 }
 
@@ -27,8 +43,10 @@ class Cat extends Animal{
     public function __construct($cry){
         parent::__construct($cry);
     }
+
+    //METHOD
     public function toCry(): string{
-        return 'Meu!';
+        return 'MEU!';
     }
 }
 
