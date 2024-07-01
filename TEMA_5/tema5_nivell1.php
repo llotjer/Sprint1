7ad70803cec6ff3acf828831fa252e70f1137880
Xavi <149520332+llotjer@users.noreply.github.com>
2024-07-01 22:47:03 +0200

    Changed almost everything. Added getters & setters. Now using interface instead of abstract class Animal.

--- TEMA_5/tema5_nivell1.php
+++ TEMA_5/tema5_nivell1.php
@@ -1,52 +1,83 @@
 <?php
 
-abstract class Animal{
+//INITIALLY, I CREATED AN ABSTRACT CLASS NAMED ANIMAL WITH AN ABSTRACT METHOD CALLED MAKESOUND. 
+//HOWEVER, I REALIZED THAT I DIDN’T NEED AN ENTIRE CLASS. I JUST NEEDED A METHOD FOR THE DOG AND CAT CLASSES 
+//TO MAKE THEIR RESPECTIVE SOUNDS. THIS CAN BE ACHIEVED USING AN INTERFACE.
+
+interface sound{
+    public function makeSound(): string;
+}
+
+
+class Dog implements sound{
+    
     //PROPERTIES
+    private $name;
     private $sound;
 
     //CONSTRUCTOR
-    public function __construct(string $sound){
-        $this->sound = $sound;
+    public function __construct($name){
+       $this->sound = 'BUP, BUP!';
+       $this->name = $name;
     }
 
     //GETTERS
+    public function getName(): string{
+        return $this->name;
+    }
 
     public function getSound(): string{
         return $this->sound;
     }
 
     //SETTERS
+    public function setName(string $name): void{
+        $this->name = $name;
+    }
 
     public function setSound(string $sound): void{
         $this->sound = $sound;
     }
 
     //METHOD
-    public abstract function makeSound();
-
+    public function makeSound(): string{
+        return $this->sound;
+    }
 }
 
-class Dog extends Animal{
+class Cat implements sound{
+
+    //PROPERTIES
+    private $name;
+    private $sound;
+
     //CONSTRUCTOR
-    public function __construct($sound){
-        parent::__construct($sound);
+    public function __construct($name){
+        $this->sound = 'MEU!';
+        $this->name = $name;
+    }
+
+    //GETTERS
+    public function getName(): string{
+        return $this->name;
+    }
+
+    public function getSound(): string{
+        return $this->sound;
+    }
+
+    //SETTERS
+    public function setName(string $name): void{
+        $this->name = $name;
+    }
+
+    public function setSound(string $sound): void{
+        $this->sound = $sound;
     }
 
     //METHOD
     public function makeSound(): string{
-        return 'BUP, BUP!';
-    }
-}
-
-class Cat extends Animal{
-
-    public function __construct($sound){
-        parent::__construct($sound);
-    }
-
-    //METHOD
-    public function makeSound(): string{
-        return 'MEU!';
+        return $this->sound;
     }
 }
 
